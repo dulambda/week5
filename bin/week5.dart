3ece0e5d630d88c1e252ef97a484abee1e9a4377
Mansur Yskak <mansur.yskak@bk.ru>
2026-02-20 12:47:42 +0500

    Mansur

--- bin/week5.dart
+++ bin/week5.dart
@@ -3,7 +3,7 @@ import 'package:week5/week5.dart' as week5;
 void main(List<String> arguments) {
   print('Hello world: ${week5.calculate()}!');
   print('Rest in');
-  print('Peace');
+  print('Peac2e3');
   print("Mansur");
   print("Mansur2");
   print("Mansur New");
